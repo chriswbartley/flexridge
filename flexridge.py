b0afe42087557c6191f4881197b1926452f66d8f
Chris Bartley <chris@commonality.me>
2023-10-19 10:49:12 +0800

    first finished commit

---
+++ flexridge.py
@@ -0,0 +1,249 @@
+from scipy.optimize import minimize
+from scipy.optimize import NonlinearConstraint, LinearConstraint
+import numpy as np
+
+class QuadraticFit:
+    critical_dip_value = 1*np.exp(-1*np.abs(0.5))
+    def __init__(self, y1, y2, y3):
+        self.y1 = y1
+        self.y2= y2
+        self.y3 = y3
+        P1,P2,P3 = self.y1,self.y2,self.y3
+        # calculate quadratic that goes through these points
+        self.a = P1
+        self.c = 0.5*(P3+P1-2*P2)
+        self.b = P2-P1-self.c
+
+        self.quad_pos_fn = lambda x_: self.a + self.b*x_ + self.c*x_**2
+        if self.c !=0:
+            self.x_min = -self.b/(2*self.c)
+        else:
+            self.x_min = np.inf
+        # precalculate jacobian components
+        self.denom = (self.y1-self.y3)**3
+        self.factor = 2*self.y1-4*self.y2+ 2* self.y3
+    def calc_value(self,x):
+        return self.quad_pos_fn(x)
+    def calc_dip_metric2(self):
+        if self.c !=0:
+            return np.sign(self.c)*1./(-self.b/2/self.c - 1)**2
+        else:
+            return 0.
+    def calc_dip_metric2_jacobian(self):
+
+        jac = np.sign(self.c)*self.factor/self.denom*np.array([
+            8*(self.y2 - self.y3),
+            8*(-self.y1 + self.y3),
+            8*(self.y1 - self.y2),
+        ])
+        return jac
+    def calc_dip_metric(self):
+        if self.c !=0:
+            return np.sign(self.c)*np.exp(-1*np.abs(-self.b/2/self.c-1))
+        else:
+            return 0.
+    def calc_dip_metric_jacobian(self):
+        val = self.calc_dip_metric()
+        jac = -1*np.sign(self.x_min-1)*np.sign(self.c)/self.factor**2*np.exp(-1*np.abs(-self.b/2/self.c-1))*np.array([
+            -4*(self.y2 - self.y3),
+            -4*(-self.y1 + self.y3),
+            -4*(self.y1 - self.y2),
+        ])
+        return jac
+
+def get_dip_values(values, return_what = 'values'):
+    if return_what == 'values':
+        metrics = []
+    else:
+        metrics = np.zeros([300,len(values)])
+    n_ = len(values)
+    i_row = 0
+    for i_start in range(n_-2):
+        for i_end in range(i_start+2, n_):
+            for i_mid in range(i_start+1, i_end):
+                # print(i_start, i_end, i_mid)
+                if return_what == 'values':
+                    metrics.append(QuadraticFit(values[i_start],
+                                         values[i_mid],
+                                         values[i_end]).calc_dip_metric())
+                else: # return jacobian
+                    jac_local = QuadraticFit(values[i_start],
+                                         values[i_mid],
+                                         values[i_end]).calc_dip_metric_jacobian()
+                    metrics[i_row,i_start] =jac_local[0]
+                    metrics[i_row,i_mid] =jac_local[1]
+                    metrics[i_row,i_end] =jac_local[2]
+
+                    i_row =i_row +1
+    if return_what !='values':
+        metrics = metrics[:i_row]
+    return np.array(metrics)
+
+class RidgeRegression():
+    def __init__(self,
+                    monotone_constraints=None, # either: None=0, -1 (all non-increasing), +1 (all non-decreasing), or a list of +1/-1/0 for all feats
+                 positive=None, # included for sklearn Ridge compatibiltiy. positive=True is equivalent to monotone_constraints=1.
+                 alpha = 0.01,
+                 fit_intercept=True,
+                 unimodal = False, # either: True/False, OR a list of lists of indices of features to be made unimodal
+                 ):
+        self.monotone_constraints = monotone_constraints
+        self.alpha = alpha
+        self.fit_intercept = fit_intercept
+        self.unimodal = unimodal
+        self.positive = positive
+
+
+    def fit(self, X, y, sample_weight=None):
+        if not self.positive is None:
+            if self.positive:
+                self.monotone_constraints = 1
+        # construct optimisation problem with requested percentiles
+        coefs_init_incl_intercept = np.zeros(X.shape[1]+(1 if self.fit_intercept else 0))
+        X_ = X.copy()
+        coef_end_idx = X_.shape[1]
+
+        if sample_weight is None:
+            sample_weight = np.ones(len(y))
+        # standardise sampleweight so that sum equals len(y)
+        sample_weight = sample_weight  / (np.sum(sample_weight)/len(y))
+        # Add intercept
+        if self.fit_intercept:
+            X_ = np.hstack([ X_,np.ones([X.shape[0],1])])
+
+        def loss_fn(coefs):
+            loss =  0.5*np.sum(sample_weight*(y-np.dot(X_,coefs))**2)+ 0.5*self.alpha*np.sum(coefs[:coef_end_idx]**2)
+            return loss
+
+        def loss_fn_with_jac(coefs):
+            loss =  0.5*np.sum(sample_weight*(y-np.dot(X_,coefs))**2)+ 0.5*self.alpha*np.sum(coefs[:coef_end_idx]**2)
+            y_resid = np.dot(X_,coefs)-y
+            jac = np.dot(X_.T, sample_weight*y_resid).ravel() +self.alpha*np.hstack([ coefs[:coef_end_idx],[0]])
+            return loss,jac
+
+        def loss_fn_with_jac_ls_coefs(coefs, reference_coefs):
+
+            loss__ =  0.5*np.sum((coefs[:-1]-reference_coefs[:-1])**2)
+            y_resid = coefs[:-1]-reference_coefs[:-1]
+            jac = np.hstack([y_resid,[0]])
+            return loss__,jac
+
+        jac = True
+        # initial unconstrained solve: L-BFGS-B is VERY fast
+        res = minimize(loss_fn, x0 = coefs_init_incl_intercept, method = 'L-BFGS-B',
+                       # options = {'maxiter':100},
+                       )
+        # update initial guess to be unconstrained fit
+        coefs_init_incl_intercept = res['x']
+        coefs_init = coefs_init_incl_intercept[:coef_end_idx]
+        constraints = []
+        if not self.monotone_constraints is None:
+            if not hasattr(self.monotone_constraints,  '__iter__'):
+                mono_constr = [self.monotone_constraints]*X.shape[1]
+            else:
+                mono_constr = self.monotone_constraints
+            if self.fit_intercept:
+                    mono_constr.append(0)
+            for i, mono_constr_ in enumerate(mono_constr):
+                A = np.zeros(len(coefs_init_incl_intercept))
+                A[i] = 1
+                if mono_constr_ == 1:
+                    constraints.append(LinearConstraint(A, lb=0., ub=np.inf, keep_feasible=False))
+                elif mono_constr_ == -1:
+                    constraints.append(LinearConstraint(A, lb=-np.inf, ub=0, keep_feasible=False))
+                elif mono_constr_ == 0:
+                    pass
+                else:
+                    raise ValueError('monotone_constraints must be 1, -1 or 0: {}'.format())
+
+
+        if not self.unimodal is None:
+            # add unimodal constraints
+            if type(self.unimodal) == bool:
+                if self.unimodal:
+                    unimodal_idx_groups = [np.arange(X.shape[1], dtype=int)]
+                else:
+                    unimodal_idx_groups = []
+            else:
+                unimodal_idx_groups = self.unimodal
+            if len(unimodal_idx_groups)>0: # we have at least one group of unimodal features
+                for unimodal_idx_grp in unimodal_idx_groups:
+                    metric_dip = np.max(get_dip_values(coefs_init[unimodal_idx_grp]))
+                    print('coefs init', coefs_init[unimodal_idx_grp])
+                    print('metric_dip',metric_dip)
+                    if metric_dip>QuadraticFit.critical_dip_value:
+                        print('adding constraint')
+                        constr_multiplier = 1
+                        constr_fn = lambda coefs__incl_int: constr_multiplier*get_dip_values(coefs__incl_int[:-1][unimodal_idx_grp], return_what = 'values')#QuadraticFit
+                        def constr_fn_jac(coefs__incl_int):
+                            res_ = get_dip_values(coefs__incl_int[:-1][unimodal_idx_grp], return_what = 'jacobian')
+                            jac_new = np.zeros([len(res_), len(coefs__incl_int)])
+                            jac_new[:,unimodal_idx_grp] = res_
+                            return jac_new
+
+                        constraints.append(NonlinearConstraint(fun=constr_fn,
+                                           lb=-np.inf, ub = constr_multiplier*QuadraticFit.critical_dip_value,
+                                           jac = constr_fn_jac,#'cs',# constr_fn_jac, #'cs',# cs seems better as providing jacobian can result in
+                                                               )
+                                           )
+
+        solve = lambda solver, maxiter, x0: minimize(loss_fn_with_jac,
+                                        jac=jac, x0 = x0,
+                       method = solver,#trust-constr',#SLSQP',#L-BFGS-B',#SLSQP', COBYLA
+                       options = {'maxiter':maxiter,
+                                  # 'ftol':1e-4,
+                                  },
+                       constraints = constraints)
+        res = solve('SLSQP',1000,coefs_init_incl_intercept)
+
+        if not res['success']:
+            print('SLSQP failed, trying interior pt')
+            # resolve init starting point
+            loss_fn_init_jac_ = lambda coef_: loss_fn_with_jac_ls_coefs(coef_, coefs_init_incl_intercept)
+
+            coefs_init__ = coefs_init_incl_intercept
+            print('init coefs: ',coefs_init_incl_intercept)
+            solve_init =  minimize(loss_fn_init_jac_,jac=True, x0 = coefs_init__,
+                       method = 'SLSQP',
+                       options = {'maxiter':1000,
+                                  # 'ftol':1e-4,
+                                  },
+                       constraints = constraints)
+            if not solve_init['success']:
+                print('####### COULD NOT SOLVE INIT COEFS')
+                print(solve_init)
+            else:
+                coefs_init_incl_intercept = solve_init['x']
+
+            res = solve('trust-constr',10000,coefs_init_incl_intercept)
+            if not res['success']:
+                print(res)
+
+        self.coef_ = res['x']
+        self.intercept_ = 0.
+        if self.fit_intercept:
+            self.intercept_ = self.coef_[-1]
+            self.coef_ = self.coef_[:coef_end_idx]
+
+
+    def predict(self, X):
+        X_ = X.copy()
+        coef_ = self.coef_
+        if self.fit_intercept:
+            X_ = np.hstack([ X_,np.ones([X.shape[0],1])])
+            coef_ = np.array(list(coef_)+[self.intercept_])
+        return np.dot(X_, coef_).ravel()
+
+
+    def get_params(self, deep=False):
+        dict = {
+            'monotone_constraints' : self.monotone_constraints,
+            'alpha': self.alpha,
+        }
+        return dict
+
+    def set_params(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        return self
+
---
+++ test_flexridge.py
@@ -0,0 +1,180 @@
+from sklearn.linear_model import Ridge
+import scipy.stats
+import time
+import numpy as np
+from flexridge import RidgeRegression, get_dip_values
+
+
+import matplotlib.pyplot as plt
+
+# trial solving constrained coefficients
+np.random.seed(102)
+n=70000
+n_feats = 6
+X = np.random.rand(n*n_feats).reshape(n,n_feats)
+sample_weight = np.random.rand(n)# np.ones(n)
+coefs_all = [np.array([0.02,0.05,0.5,-.4,0.25,0.1]),
+            np.array([0.02,0.05,0.5,.4,0.25,0.1]),
+         ]
+unimodal_idx_groups = False
+fit_intercept = True
+alpha=0.01
+n_trials = 10
+
+def test_standard_fit_matches_sklearn():
+    for i_coefs, coefs in enumerate(coefs_all):
+        print('true coef:', coefs)
+        y = np.dot(X, coefs
+                   ).ravel() + scipy.stats.norm(0,1).rvs(n)
+
+        # simple least squares
+        start = time.time()
+        for i in range(n_trials):
+            reg = Ridge(alpha=alpha,#solver='lsqr', #normalize=False,
+                        fit_intercept=fit_intercept,# positive=True
+                        )
+            reg.fit(X,y,sample_weight=sample_weight)
+        time_s = np.round(time.time()-start,3)
+        print('std ridge:',0.5*np.sum((y-reg.predict(X))**2), time_s, reg.intercept_,reg.coef_,
+              )
+
+        # custom Ridge
+        start = time.time()
+        for i in range(n_trials):
+            reg_custom = RidgeRegression(alpha=alpha,
+                                         fit_intercept=fit_intercept,
+                        unimodal=unimodal_idx_groups)
+            reg_custom.fit(X,y,sample_weight=sample_weight)
+        time_s = np.round(time.time()-start,3)
+        print('custom ridge:',0.5*np.sum((y-reg_custom.predict(X))**2),
+              time_s,
+              reg.intercept_, reg_custom.coef_,
+              )
+        # print(reg.intercept_, reg_custom.intercept_)
+        assert np.allclose(reg.intercept_, reg_custom.intercept_,atol=1e-4)
+        assert np.allclose(reg.coef_, reg_custom.coef_,atol=1e-4)
+        assert np.allclose(reg.predict(X), reg_custom.predict(X),atol=1e-4)
+
+def test_monotone_fit_matches_sklearn():
+    for i_coefs, coefs in enumerate(coefs_all):
+        y = np.dot(X, coefs
+                   ).ravel() + scipy.stats.norm(0, 1).rvs(n)
+        # simple least squares
+        start = time.time()
+        for i in range(n_trials):
+            reg = Ridge(alpha=alpha,#solver='lsqr', #normalize=False,
+                        fit_intercept=fit_intercept, positive=True
+                        )
+            reg.fit(X,y,sample_weight=sample_weight)
+        time_s = np.round(time.time()-start,3)
+        print('std ridge monotone:',0.5*np.sum((y-reg.predict(X))**2), time_s, reg.intercept_,reg.coef_,
+              )
+
+        # custom Ridge
+        start = time.time()
+        for i in range(n_trials):
+            reg_custom = RidgeRegression(alpha=alpha,
+                                         fit_intercept=fit_intercept,
+                        unimodal=unimodal_idx_groups,
+                                         monotone_constraints=[1]*(len(coefs)))
+            reg_custom.fit(X,y,sample_weight=sample_weight)
+        time_s = np.round(time.time()-start,3)
+        print('custom ridge monotone:',0.5*np.sum((y-reg_custom.predict(X))**2),
+              time_s,
+              reg.intercept_, reg_custom.coef_,
+              )
+        assert np.allclose(reg.intercept_, reg_custom.intercept_,atol=1e-4)
+        assert np.allclose(reg.coef_, reg_custom.coef_,atol=1e-4)
+        assert np.allclose(reg.predict(X), reg_custom.predict(X),atol=1e-4)
+
+def test_unimodal_solutions():
+    correct_unimodal_coefs = [np.array([-0.15253716,  0.27612617,  0.27612618,  0.82850476, -0.22418673, -0.26600565]),
+                np.array([-0.04950415, -0.04950415,  0.50304867,  0.50865376,  0.00694265, -0.12748278]),
+                np.array([-0.04950671, -0.04950671,  0.50305287,  0.50865698,  0.25691133, -0.12748643]),
+                np.array([-0.09941247, -0.09941247,  0.28908646,  0.28908646,  0.28908646,  0.36963751]),
+                np.array([-0.13743962, -0.13743962,  0.50099497,  0.50307497,  0.01111545, -0.13402668]),
+                np.array([0.61969394 , 0.33860653 , 0.33860642 , 0.33860633 , 0.27177542 , 0.2717753 ]),
+                np.array([-0.05994643,  0.1801058 ,  0.70110524,  0.26040426 , 0.25248045,  0.25248045]),
+                np.array([0.73636598 , 0.73636598 , 0.90922988 , 0.99976239 , 0.49803944 , 0.49803944]),
+                np.array([0.42534316 , 0.60668949 , 0.78069408 , 0.33134332 , 0.33134333 , 0.15102768]),
+                ]
+
+    # trial solving constrained coefficients
+    rand_seed = 121
+    n = 900
+    n_feats = 6
+    np.random.seed(rand_seed)
+    X = np.random.rand(n * n_feats).reshape(n, n_feats)
+    coefs_all = [
+        np.array([0., 0.75, 0., .75, 0., 0]),
+        np.array([0.2, 0.05, 0.5, .4, 0.25, 0.1]),
+        np.array([0.2, 0.05, 0.5, .4, 0.5, 0.1]),
+        np.array([0.1, 0.05, 0.3, .2, 0.5, 0.6]),
+        np.array([0.02, 0.05, 0.5, .4, 0.25, 0.1]),  # prob
+        np.random.rand(6),
+        np.random.rand(6),
+        np.random.rand(6),
+        np.random.rand(6),
+    ]
+    # unimodal_idx_groups = True
+    fit_intercept = True
+    alpha = 0.01
+    for i_coefs, coefs in enumerate(coefs_all):
+        np.random.seed(rand_seed)  # +i_coefs)
+        print('true coef:', coefs)
+
+        y = np.dot(X, coefs
+                   ).ravel() + scipy.stats.norm(0, 1).rvs(n)
+
+        # custom Ridge
+        start = time.time()
+        # for i in range(100):
+        reg_custom = RidgeRegression(alpha=alpha,
+                                     fit_intercept=fit_intercept,
+                                     unimodal=False)
+        reg_custom.fit(X, y)
+        time_s = np.round(time.time() - start, 3)
+        print('custom ridge:', 0.5 * np.sum((y - reg_custom.predict(X)) ** 2), np.max(get_dip_values(reg_custom.coef_)),
+              time_s,
+              reg_custom.intercept_, reg_custom.coef_,
+              )
+        fig, ax = plt.subplots()
+        start = time.time()
+        # for i in range(100):
+        reg_custom_unimodal = RidgeRegression(alpha=alpha,
+                                              fit_intercept=fit_intercept,
+                                              unimodal=True)
+        reg_custom_unimodal.fit(X, y)
+        time_s = np.round(time.time() - start, 3)
+        print('custom ridge unimodal:', 0.5 * np.sum((y - reg_custom_unimodal.predict(X)) ** 2),
+              np.max(get_dip_values(reg_custom_unimodal.coef_)),
+              time_s,
+              reg_custom_unimodal.intercept_, reg_custom_unimodal.coef_,
+              )
+        #
+        X_coef_locn = np.arange(len(coefs))
+
+        plt.plot(X_coef_locn, coefs, marker='o', label='coefs true', color='k', linestyle='--')
+        # plt.plot(X_coef_locn,coefs_est_ls, color='r',label='coefs ls', marker='x')
+        plt.plot(X_coef_locn, reg_custom.coef_, label='coefs ridge', color='red', marker='x')
+        plt.plot(X_coef_locn, reg_custom_unimodal.coef_, label='coefs ridge unimodal',
+                 color='blue', marker='o', linestyle='--')
+        print('unimodal coefs:', reg_custom_unimodal.coef_)
+        # plt.plot(X_coef_locn,coefs_est_sp_uni, label='coefs spline constrained unimodal', color='g', marker='p')
+        # plt.plot(x_all,coefs_full_spline_uni, label='coefs spline constrained unimodal', color='g', linestyle='--')
+        # print(np.dot(cs.derivations_mat,spl_coefs_opt_uni ))
+        # print(np.dot(constraints[-2,:],spl_coefs_opt_uni ))
+        plt.legend()
+        plt.grid()
+        plt.ylim([0, 1])
+        plt.show()
+
+        # print('XX',correct_unimodal_coefs[i_coefs], reg_custom_unimodal.coef_)
+        assert(np.allclose(correct_unimodal_coefs[i_coefs], reg_custom_unimodal.coef_))
+
+
+if __name__ == '__main__':
+    test_unimodal_solutions()
+    test_standard_fit_matches_sklearn()
+    test_monotone_fit_matches_sklearn()
+
